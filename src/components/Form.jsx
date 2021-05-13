83d48e486789c150f40e0806f35324b0a54ae774
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2021-05-13 10:58:13 +0100

    updates

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -21,7 +21,7 @@ const Form = ({ input, setInput, todos, setTodos }) => {
       <div
         className="card"
         style={{
-          width: "35rem",
+          width: "36rem",
           background: " rgba( 255, 255, 255, 0.25 )",
           boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
           backdropFilter: "blur( 4px )",
